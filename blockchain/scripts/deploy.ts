ab87598613b573646789e9dc00c231e93b21a0fd
Rei Sugiura <reibomaru@gmail.com>
2022-06-29 17:21:24 +0900

    feat: add deploy script

--- blockchain/scripts/deploy.ts
+++ blockchain/scripts/deploy.ts
@@ -3,7 +3,7 @@
 //
 // When running the script with `npx hardhat run <script>` you'll find the Hardhat
 // Runtime Environment's members available in the global scope.
-import { ethers } from "hardhat";
+import { ethers, upgrades } from "hardhat";
 
 async function main() {
   // Hardhat always runs the compile task when running scripts with its command
@@ -14,12 +14,13 @@ async function main() {
   // await hre.run('compile');
 
   // We get the contract to deploy
-  const Greeter = await ethers.getContractFactory("Greeter");
-  const greeter = await Greeter.deploy("Hello, Hardhat!");
+  const Knowtfolio = await ethers.getContractFactory("Knowtfolio");
+  const knowtfolio = await upgrades.deployProxy(Knowtfolio, [], {
+    initializer: "initialize",
+  });
+  await knowtfolio.deployed();
 
-  await greeter.deployed();
-
-  console.log("Greeter deployed to:", greeter.address);
+  console.log("Greeter deployed to:", knowtfolio.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
