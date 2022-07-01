67e54781feaa8ade6222ffd04d185b69a05225ed
Rei Sugiura <reibomaru@gmail.com>
2022-07-01 12:12:48 +0900

    lib: add npm scripts

--- blockchain/hardhat.config.ts
+++ blockchain/hardhat.config.ts
@@ -26,6 +26,13 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 const config: HardhatUserConfig = {
   solidity: "0.8.4",
   networks: {
+    localhost: {
+      url: "http://127.0.0.1:8545/",
+      chainId: 31337,
+      accounts: [
+        "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80",
+      ],
+    },
     mumbai: {
       url: process.env.MUMBAI_URL || "",
       accounts:
