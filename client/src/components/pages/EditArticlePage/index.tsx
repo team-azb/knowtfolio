5ac63d12dde7731c3db4753f19f772863d45adb9
Rei Sugiura <reibomaru@gmail.com>
2022-07-03 00:46:11 +0900

    feat: implemented edit page

--- client/src/components/pages/EditArticlePage/index.tsx
+++ client/src/components/pages/EditArticlePage/index.tsx
@@ -2,16 +2,45 @@ import { useParams } from "react-router-dom";
 import { Editor } from "@tinymce/tinymce-react";
 import { Editor as TinyMCEEditor } from "tinymce";
 import { TINY_MCE_API_KEY } from "~/configs/tinymce";
-import { useCallback, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
+import { getArticle, updateArticle } from "~/apis/knowtfolio";
+import { useWeb3 } from "~/components/organisms/Web3Provider";
 
 const EditArticlePage = () => {
   const { articleId } = useParams();
   const [content, setContent] = useState("");
+  const [title, setTitle] = useState("");
   const handleEditorChange = useCallback<
     (a: string, editor: TinyMCEEditor) => void
   >((value) => {
     setContent(value);
   }, []);
+  const { web3, account } = useWeb3();
+
+  useEffect(() => {
+    (async () => {
+      if (articleId) {
+        const { title, content } = await getArticle(articleId);
+        setTitle(title);
+        setContent(content);
+      }
+    })();
+  }, [articleId]);
+
+  const handleUpdate = useCallback(async () => {
+    const signature = await web3.eth.personal.sign(
+      "Update Article",
+      account,
+      ""
+    );
+    updateArticle({
+      articleId: articleId || "",
+      title,
+      content,
+      address: account,
+      signature,
+    });
+  }, [account, articleId, content, title, web3.eth.personal]);
   return (
     <>
       <h1>Edit Articles: {articleId}</h1>
@@ -28,6 +57,7 @@ const EditArticlePage = () => {
       />
       <p>preview</p>
       <div>{content}</div>
+      <button onClick={handleUpdate}>update article</button>
     </>
   );
 };
