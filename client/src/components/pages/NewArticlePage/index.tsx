676e69261da6c42a7ed27fa17e4368438d995cb9
Rei Sugiura <reibomaru@gmail.com>
2022-07-02 14:50:26 +0900

    feat: add new article page

--- client/src/components/pages/NewArticlePage/index.tsx
+++ client/src/components/pages/NewArticlePage/index.tsx
@@ -1,5 +1,33 @@
+import { Editor } from "@tinymce/tinymce-react";
+import { Editor as TinyMCEEditor } from "tinymce";
+import { TINY_MCE_API_KEY } from "~/configs/tinymce";
+import { useCallback, useState } from "react";
+
 const NewArticlePage = () => {
-  return <h1>New Aritcle</h1>;
+  const [content, setContent] = useState("");
+  const handleEditorChange = useCallback<
+    (a: string, editor: TinyMCEEditor) => void
+  >((value) => {
+    setContent(value);
+  }, []);
+  return (
+    <>
+      <h1>New Aritcle</h1>
+      <Editor
+        onEditorChange={handleEditorChange}
+        value={content}
+        apiKey={TINY_MCE_API_KEY}
+        init={{
+          height: 500,
+          menubar: true,
+          content_style:
+            "body { font-family:Helvetica,Arial,sans-serif; font-size:14px }",
+        }}
+      />
+      <p>preview</p>
+      <div>{content}</div>
+    </>
+  );
 };
 
 export default NewArticlePage;
