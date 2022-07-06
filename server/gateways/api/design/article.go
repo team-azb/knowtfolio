4f60589bb9b930f1ef00f5f929fdb53b08afcf84
shugo256 <shugo256@gmail.com>
2022-07-06 22:43:29 +0900

    Design authorization.

--- server/gateways/api/design/article.go
+++ server/gateways/api/design/article.go
@@ -28,15 +28,16 @@ func articleContentAttribute(fieldName string) {
 	})
 }
 
-var articleIdPayload = dsl.Type("ArticleId", func() {
+var articleReadRequest = dsl.Type("ArticleReadRequest", func() {
 	articleIdAttribute("id")
 	dsl.Required("id")
 })
 
-var articleCreateRequest = dsl.Type("articleCreateRequest", func() {
+var articleCreateRequest = dsl.Type("ArticleCreateRequest", func() {
 	articleTitleAttribute("title")
 	articleContentAttribute("content")
-	dsl.Required("title", "content")
+	smartWalletAuthAttributes("address", "signature")
+	dsl.Required("title", "content", "address", "signature")
 })
 
 var articleUpdateRequest = dsl.Type("ArticleUpdateRequest", func() {
@@ -45,8 +46,15 @@ var articleUpdateRequest = dsl.Type("ArticleUpdateRequest", func() {
 	// Body
 	articleTitleAttribute("title")
 	articleContentAttribute("content")
+	smartWalletAuthAttributes("address", "signature")
 
-	dsl.Required("id")
+	dsl.Required("id", "address", "signature")
+})
+
+var articleDeleteRequest = dsl.Type("ArticleDeleteRequest", func() {
+	articleIdAttribute("id")
+	smartWalletAuthAttributes("address", "signature")
+	dsl.Required("id", "address", "signature")
 })
 
 var article = dsl.Type("Article", func() {
@@ -56,7 +64,7 @@ var article = dsl.Type("Article", func() {
 	dsl.Required("id", "title", "content")
 })
 
-var articleResult = dsl.ResultType("application/json", "ArticleResult", func() {
+var articleResult = dsl.ResultType("article-result", "ArticleResult", func() {
 	dsl.Extend(article)
 
 	dsl.View("default", func() {
@@ -72,7 +80,7 @@ var articleResult = dsl.ResultType("application/json", "ArticleResult", func() {
 var _ = dsl.Service("articles", func() {
 	dsl.Description("記事サービス")
 
-	dsl.Error("not_found")
+	dsl.Error("article_not_found")
 
 	dsl.HTTP(func() {
 		dsl.Path("/articles")
@@ -83,7 +91,10 @@ var _ = dsl.Service("articles", func() {
 
 		dsl.Payload(articleCreateRequest, "作成したい記事の情報")
 
-		dsl.Result(articleResult)
+		dsl.Result(articleResult, func() {
+			dsl.View("default")
+		})
+		dsl.Error("unauthenticated")
 
 		dsl.HTTP(func() {
 			dsl.POST("/")
@@ -95,17 +106,16 @@ var _ = dsl.Service("articles", func() {
 	dsl.Method("Read", func() {
 		dsl.Description("Get article by id.")
 
-		dsl.Payload(articleIdPayload)
+		dsl.Payload(articleReadRequest)
 
-		dsl.Result(articleResult)
+		dsl.Result(articleResult, func() {
+			dsl.View("default")
+		})
 
 		dsl.HTTP(func() {
 			dsl.GET("/{id}")
 
 			dsl.Response(dsl.StatusOK)
-			dsl.Response(dsl.StatusNotFound, "not_found", func() {
-				dsl.Description("IDに対応する記事が見つからなかった場合。")
-			})
 		})
 	})
 
@@ -114,34 +124,34 @@ var _ = dsl.Service("articles", func() {
 
 		dsl.Payload(articleUpdateRequest, "記事の更新内容\nリクエストに含まれるフィールドだけ更新される。")
 
-		dsl.Result(articleResult)
+		dsl.Result(articleResult, func() {
+			dsl.View("default")
+		})
+		dsl.Error("unauthenticated")
+		dsl.Error("unauthorized")
 
 		dsl.HTTP(func() {
 			dsl.PUT("/{id}")
 
 			dsl.Response(dsl.StatusOK)
-			dsl.Response(dsl.StatusNotFound, "not_found", func() {
-				dsl.Description("IDに対応する記事が見つからなかった場合。")
-			})
 		})
 	})
 
 	dsl.Method("Delete", func() {
 		dsl.Description("Delete article by id.")
 
-		dsl.Payload(articleIdPayload)
+		dsl.Payload(articleDeleteRequest)
 
 		dsl.Result(articleResult, func() {
 			dsl.View("only-id")
 		})
+		dsl.Error("unauthenticated")
+		dsl.Error("unauthorized")
 
 		dsl.HTTP(func() {
 			dsl.DELETE("/{id}")
 
 			dsl.Response(dsl.StatusOK)
-			dsl.Response(dsl.StatusNotFound, "not_found", func() {
-				dsl.Description("IDに対応する記事が見つからなかった場合。")
-			})
 		})
 	})
 })
