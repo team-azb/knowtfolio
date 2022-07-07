7733aec925ec72e06b8ae9b8f2b75829510ced62
shugo256 <shugo256@gmail.com>
2022-07-07 19:13:38 +0900

    Add abstract contract to avoid diamond inheritance errors.

---
+++ blockchain/contracts/ERC721URIStorageEnumerableUpgradeable.sol
@@ -0,0 +1,50 @@
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts-upgradeable/token/ERC721/ERC721Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC721/extensions/ERC721EnumerableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC721/extensions/ERC721URIStorageUpgradeable.sol";
+
+/**
+ * @dev Abstract contract to resolve function conflicts due to diamond inheritance.
+ */
+abstract contract ERC721URIStorageEnumerableUpgradeable is ERC721URIStorageUpgradeable, ERC721EnumerableUpgradeable {
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 tokenId
+    )
+        internal
+        virtual
+        override(ERC721Upgradeable, ERC721EnumerableUpgradeable)
+    {
+        ERC721EnumerableUpgradeable._beforeTokenTransfer(from, to, tokenId);
+    }
+
+    function _burn(uint256 tokenId)
+        internal
+        virtual
+        override(ERC721Upgradeable, ERC721URIStorageUpgradeable)
+    {
+        ERC721URIStorageUpgradeable._burn(tokenId);
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        virtual
+        override(ERC721Upgradeable, ERC721EnumerableUpgradeable)
+        returns (bool)
+    {
+        return ERC721EnumerableUpgradeable.supportsInterface(interfaceId);
+    }
+
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        virtual
+        override(ERC721Upgradeable, ERC721URIStorageUpgradeable)
+        returns (string memory)
+    {
+        return ERC721URIStorageUpgradeable.tokenURI(tokenId);
+    }
+}
