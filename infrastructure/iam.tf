a03e5975bd92c4d5794f07a1ea8caf0960135845
Rei Sugiura <reibomaru@gmail.com>
2022-07-13 16:39:46 +0900

    feat: nft保存用バケットを操作できるadminユーザーの作成

---
+++ infrastructure/iam.tf
@@ -0,0 +1,15 @@
+resource "aws_iam_user" "knowtfolio_admin" {
+  name = "knowtfolio_admin"
+}
+
+resource "aws_iam_access_key" "knowtfolio_admin" {
+  user = aws_iam_user.knowtfolio_admin.name
+}
+
+resource "aws_iam_user_policy" "knowtfolio_nft_io" {
+  name = "knowtfolio-nft-io"
+  user = aws_iam_user.knowtfolio_admin.name
+  policy = templatefile("${path.module}/templates/iam/s3_io_iam_policy.json", {
+    resource = "${aws_s3_bucket.knowtfolio_alpha.arn}/nfts/*"
+  })
+}
