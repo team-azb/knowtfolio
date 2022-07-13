5f6a236ee7064f9e20374c97715ca843efae8d66
Rei Sugiura <reibomaru@gmail.com>
2022-07-13 12:44:10 +0900

    feat: cognitoのschemaを実装

--- infrastructure/cognito.tf
+++ infrastructure/cognito.tf
@@ -1,3 +1,23 @@
 resource "aws_cognito_user_pool" "knowfolio" {
   name = "knowtfolio"
+  admin_create_user_config {
+    allow_admin_create_user_only = false
+  }
+  schema {
+    name                = "email"
+    attribute_data_type = "String"
+    required            = true
+  }
+
+  schema {
+    name                = "wallet_address"
+    attribute_data_type = "String"
+    mutable             = true
+    string_attribute_constraints {
+      min_length = 1
+      max_length = 256
+    }
+  }
+
+  auto_verified_attributes = ["email"]
 }
