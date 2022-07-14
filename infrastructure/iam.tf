d3c5d48f40b6fafd47e284502a4aaf13de888dd6
Rei Sugiura <reibomaru@gmail.com>
2022-07-14 11:03:21 +0900

    refactor: change bucket name

--- infrastructure/iam.tf
+++ infrastructure/iam.tf
@@ -10,6 +10,6 @@ resource "aws_iam_user_policy" "knowtfolio_nft_io" {
   name = "knowtfolio-nft-io"
   user = aws_iam_user.knowtfolio_admin.name
   policy = templatefile("${path.module}/templates/iam/s3_io_iam_policy.json", {
-    resource = "${aws_s3_bucket.knowtfolio_alpha.arn}/nfts/*"
+    resource = "${aws_s3_bucket.knowtfolio_resources.arn}/nfts/*"
   })
 }
