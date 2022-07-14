36cf494ae327d362c30fef7066bb3ed9757935f7
Rei Sugiura <reibomaru@gmail.com>
2022-07-14 16:55:35 +0900

    refactor: move templates directory under src/

--- infrastructure/iam.tf
+++ infrastructure/iam.tf
@@ -9,7 +9,7 @@ resource "aws_iam_access_key" "knowtfolio_admin" {
 resource "aws_iam_user_policy" "knowtfolio_nft_io" {
   name = "knowtfolio-nft-io"
   user = aws_iam_user.knowtfolio_admin.name
-  policy = templatefile("${path.module}/templates/iam/s3_io_iam_policy.json", {
+  policy = templatefile("${path.module}/src/templates/iam/s3_io_iam_policy.json", {
     resource = "${aws_s3_bucket.knowtfolio_resources.arn}/nfts/*"
   })
 }
