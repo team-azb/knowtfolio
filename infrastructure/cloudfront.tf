fe4005b7eb70ee0305b825888054aed49c0d0163
Rei Sugiura <reibomaru@gmail.com>
2022-07-14 16:53:37 +0900

    feat: implemented cloudfront function; append .json

--- infrastructure/cloudfront.tf
+++ infrastructure/cloudfront.tf
@@ -34,13 +34,17 @@ resource "aws_cloudfront_distribution" "knowtfolio" {
   }
 
   ordered_cache_behavior {
-    path_pattern           = "/nfts/*.json"
+    path_pattern           = "/nfts/*"
     allowed_methods        = ["GET", "HEAD", "OPTIONS"]
     cached_methods         = ["GET", "HEAD", "OPTIONS"]
     viewer_protocol_policy = "redirect-to-https"
     target_origin_id       = aws_s3_bucket.knowtfolio_resources.id
     cache_policy_id        = "658327ea-f89d-4fab-a63d-7e88639e58f6" // CachingOptimized
     compress               = true
+    function_association {
+      event_type   = "viewer-request"
+      function_arn = aws_cloudfront_function.append_dot_json.arn
+    }
   }
 
   # TODO: SPAのルーティングの方法について考え直す
@@ -70,4 +74,11 @@ resource "aws_cloudfront_origin_access_identity" "knowtfolio" {
 
 resource "aws_cloudfront_origin_access_identity" "knowtfolio_resources" {
   comment = "origin access identity for s3 knowtfolio-resources"
+}
+
+resource "aws_cloudfront_function" "append_dot_json" {
+  name    = "append_dot_json"
+  runtime = "cloudfront-js-1.0"
+  publish = true
+  code    = file("${path.module}/src/function_scripts/appendDotJson.js")
 }
