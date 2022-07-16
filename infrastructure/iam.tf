f02874e38af12af328f6424bba401df31ea44bad
Rei Sugiura <reibomaru@gmail.com>
2022-07-16 11:56:27 +0900

    feat: add cors setting to s3 knowtfolio

--- infrastructure/iam.tf
+++ infrastructure/iam.tf
@@ -16,7 +16,7 @@ resource "aws_iam_user_policy" "knowtfolio_nft_io" {
 
 resource "aws_iam_role" "knowtfolio_article_writer" {
   name = "knowtfolio-article-writer"
-  assume_role_policy = templatefile("${path.module}/src/templates/iam/knowtfolio_user_assume_policy.json", {
+  assume_role_policy = templatefile("${path.module}/templates/iam/knowtfolio_user_assume_policy.json", {
     aud = aws_cognito_identity_pool.knowtfolio.id
   })
 }
@@ -24,14 +24,14 @@ resource "aws_iam_role" "knowtfolio_article_writer" {
 resource "aws_iam_role_policy" "knowtfolio_put_article_images_policy" {
   name = "put-article-images-policy"
   role = aws_iam_role.knowtfolio_article_writer.name
-  policy = templatefile("${path.module}/src/templates/iam/put_object_to_s3_policy.json", {
+  policy = templatefile("${path.module}/templates/iam/put_object_to_s3_policy.json", {
     resource = "${aws_s3_bucket.knowtfolio.arn}/images/*"
   })
 }
 
 resource "aws_iam_role" "knowtfolio_viewer" {
   name = "knowtfolio-viewer"
-  assume_role_policy = templatefile("${path.module}/src/templates/iam/knowtfolio_user_assume_policy.json", {
+  assume_role_policy = templatefile("${path.module}/templates/iam/knowtfolio_user_assume_policy.json", {
     aud = aws_cognito_identity_pool.knowtfolio.id
   })
 }
