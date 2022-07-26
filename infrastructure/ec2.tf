e54602470d8e33109eab0cdd112a4a6bd2d40f2f
Rei Sugiura <reibomaru@gmail.com>
2022-07-26 21:29:38 +0900

    refactor: add http and https gate to sg of backend ec2

--- infrastructure/ec2.tf
+++ infrastructure/ec2.tf
@@ -33,6 +33,13 @@ resource "aws_security_group" "knowtfolio_backend_ec2" {
   vpc_id = aws_vpc.knowtfolio.id
 
   ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
@@ -42,6 +49,20 @@ resource "aws_security_group" "knowtfolio_backend_ec2" {
   egress {
     from_port   = 80
     to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 443
+    to_port     = 443
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
