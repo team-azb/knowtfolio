22cdb6420839148db34b5408ca04902bc74dbefa
Rei Sugiura <reibomaru@gmail.com>
2022-07-26 15:20:38 +0900

    feat: created ec2 instance for knowtfolio backend

---
+++ infrastructure/ec2.tf
@@ -0,0 +1,48 @@
+locals {
+  UBUNTU_20_AMI = "ami-088da9557aae42f39"
+}
+
+resource "aws_key_pair" "knowtfolio" {
+  key_name   = "knowtfolio"
+  public_key = file("${path.module}/public_keys/knowtfolio.pub")
+}
+
+resource "aws_instance" "knowtfolio_backend" {
+  ami                         = local.UBUNTU_20_AMI
+  instance_type               = "t2.medium"
+  subnet_id                   = aws_subnet.knowtfolio_public_a.id
+  key_name                    = aws_key_pair.knowtfolio.id
+  associate_public_ip_address = true
+
+  root_block_device {
+    volume_type = "gp2"
+    volume_size = 30
+  }
+
+  vpc_security_group_ids = [aws_security_group.knowtfolio_backend.id]
+
+  tags = {
+    "Name" = "dev-knowtfolio-backend"
+  }
+
+  user_data = file("${path.module}/user_data/knowtfolio_backend.sh")
+}
+
+resource "aws_security_group" "knowtfolio_backend" {
+  name   = "knowtfolio-backend"
+  vpc_id = aws_vpc.knowtfolio.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
