516a9db9ef957b1ea2143940d193eafae93ff78a
Rei Sugiura <reibomaru@gmail.com>
2022-07-26 21:30:07 +0900

    refactor: add docker settings to user_data

--- infrastructure/user_data/knowtfolio_backend.sh
+++ infrastructure/user_data/knowtfolio_backend.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 sudo apt update -y
 sudo apt install make -y
+sudo curl -L "https://github.com/docker/compose/releases/download/v2.6.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo usermod -aG docker ${USER}
+sudo systemctl restart docker
