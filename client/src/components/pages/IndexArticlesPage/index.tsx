534ca5847687d8ec5124f13bb69cd88fff302681
Rei Sugiura <reibomaru@gmail.com>
2022-09-01 15:18:04 +0900

    refactor: artcileCardとcardListをコンポーネント切り出し

--- client/src/components/pages/IndexArticlesPage/index.tsx
+++ client/src/components/pages/IndexArticlesPage/index.tsx
@@ -1,29 +1,11 @@
-import { useEffect, useState } from "react";
-import { searchArticles, SearchResultEntry } from "~/apis/knowtfolio";
-import { truncate } from "./helper";
+import ArticleCardList from "~/components/organisms/ArticleCardList";
 
 const IndexArticlesPage = () => {
-  const [articles, setArticles] = useState<SearchResultEntry[]>([]);
-  useEffect(() => {
-    (async () => {
-      const { results } = await searchArticles();
-      setArticles(results);
-    })();
-  }, []);
 
   return (
     <>
       <h1>articles</h1>
-      <ul>
-        {articles.map((article) => {
-          return (
-            <li key={article.id}>
-              <a href={`articles/${article.id}`}>{article.title}</a>
-              (owner: <strong>{truncate(article.owner_address, 8, 4)}</strong>)
-            </li>
-          );
-        })}
-      </ul>
+      <ArticleCardList />
     </>
   );
 };
