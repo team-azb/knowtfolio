5075cd46bf9e51eaa45ed40c866effc0b539330d
Rei Sugiura <reibomaru@gmail.com>
2022-09-26 16:36:54 +0900

    refactor: radioボタンを使ったwalletのresetを実装

--- client/src/components/pages/ResetWalletPage/index.tsx
+++ client/src/components/pages/ResetWalletPage/index.tsx
@@ -1,9 +1,12 @@
 import { CognitoUserAttribute } from "amazon-cognito-identity-js";
 import { useCallback, useMemo, useState } from "react";
 import { useAuthContext } from "~/components/organisms/providers/AuthProvider";
+import { useWeb3Context } from "~/components/organisms/providers/Web3Provider";
 
 const ResetWalletPage = () => {
-  const [walletAddressInput, setWalletAddressInput] = useState("");
+  const [walletAddressInput, setWalletAddressInput] = useState<string | null>(
+    null
+  );
   const { user, attributes } = useAuthContext();
   const walletAddress = useMemo(() => {
     const walletAddress = attributes.find(
@@ -11,42 +14,70 @@ const ResetWalletPage = () => {
     )?.Value;
     return walletAddress;
   }, [attributes]);
+  const { account } = useWeb3Context();
+
+  const resetWalletAddress = useCallback(() => {
+    if (walletAddressInput) {
+      user.updateAttributes(
+        [
+          new CognitoUserAttribute({
+            Name: "custom:wallet_address",
+            Value: walletAddressInput,
+          }),
+        ],
+        (err) => {
+          if (err) {
+            alert(err.message || JSON.stringify(err));
+            return;
+          }
+          alert("wallet addressの更新に成功しました");
+          window.location.reload();
+        }
+      );
+    }
+  }, [user, walletAddressInput]);
 
   const changeWalletAddressInput = useCallback<
     React.ChangeEventHandler<HTMLInputElement>
-  >((event) => {
-    setWalletAddressInput(event.target.value);
-  }, []);
-
-  const resetWalletAddress = useCallback(() => {
-    user.updateAttributes(
-      [
-        new CognitoUserAttribute({
-          Name: "custom:wallet_address",
-          Value: walletAddressInput,
-        }),
-      ],
-      (err) => {
-        if (err) {
-          alert(err.message || JSON.stringify(err));
-          return;
-        }
-        alert("wallet addressの更新に成功しました");
-        window.location.reload();
+  >(
+    (event) => {
+      if (event.target.id === "wallet_address") {
+        setWalletAddressInput(account);
+      } else {
+        setWalletAddressInput(null);
       }
-    );
-  }, [user, walletAddressInput]);
+    },
+    [account]
+  );
+
   return (
     <div>
       <h2>wallet addressを再設定する</h2>
       <p>現在のwallet address: {walletAddress || "未登録"}</p>
       <div>
-        新しいwallet address:{" "}
-        <input
-          type="text"
-          placeholder="new wallet address"
-          onChange={changeWalletAddressInput}
-        />
+        新しいwallet address:
+        <div>
+          <input
+            type="radio"
+            name="wallet"
+            id="wallet_address"
+            checked={walletAddressInput !== null}
+            onChange={changeWalletAddressInput}
+          />
+          <label htmlFor="wallet_address">
+            <b>{account}</b>を登録する
+          </label>
+        </div>
+        <div>
+          <input
+            type="radio"
+            name="wallet"
+            id="none"
+            checked={walletAddressInput === null}
+            onChange={changeWalletAddressInput}
+          />
+          <label htmlFor="none">wallet addressを登録解除する</label>
+        </div>
       </div>
       <button onClick={resetWalletAddress}>update</button>
     </div>
