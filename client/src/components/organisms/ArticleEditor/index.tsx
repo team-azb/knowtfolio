77ed7a61ab8fd6ab4f4969d2e97ce729801a7696
Rei Sugiura <reibomaru@gmail.com>
2022-10-06 12:57:52 +0900

    feat: imageUploadHandlerのリファクタ

--- client/src/components/organisms/ArticleEditor/index.tsx
+++ client/src/components/organisms/ArticleEditor/index.tsx
@@ -1,17 +1,43 @@
 import { PutObjectCommand } from "@aws-sdk/client-s3";
 import { Editor } from "@tinymce/tinymce-react";
 import { Editor as TinyMCEEditor } from "tinymce";
+import { nanoid } from "nanoid";
 import { useS3Client } from "~/apis/s3";
 import { ARTICLE_RESOURCES_S3_BUCKET } from "~/configs/s3";
 import { TINY_MCE_API_KEY } from "~/configs/tinymce";
+import { useCallback } from "react";
 
 type articleEditorProps = {
   onEditorChange?: (a: string, editor: TinyMCEEditor) => void;
   value?: string;
 };
 
+const extractExtension = (filename: string) => {
+  const arr = filename.split(".");
+  return arr.length > 0 ? `.${arr.pop()}` : "";
+};
+
 const ArticleEditor = ({ onEditorChange, value }: articleEditorProps) => {
   const s3Client = useS3Client();
+
+  const imageUploadHandler = useCallback(
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    async (blobInfo: any) => {
+      const timestamp = new Date().getTime();
+      const filename = `${timestamp}_${nanoid(6)}${extractExtension(
+        blobInfo.filename()
+      )}`;
+      const command = new PutObjectCommand({
+        Bucket: ARTICLE_RESOURCES_S3_BUCKET,
+        Key: `images/${filename}`,
+        Body: blobInfo.blob(),
+        ContentType: blobInfo.blob().type,
+      });
+      await s3Client.send(command);
+      return encodeURI(`https://knowtfolio.com/images/${filename}`);
+    },
+    [s3Client]
+  );
 
   return (
     <Editor
@@ -23,18 +49,7 @@ const ArticleEditor = ({ onEditorChange, value }: articleEditorProps) => {
         menubar: true,
         content_style:
           "body { font-family:Helvetica,Arial,sans-serif; font-size:14px }",
-        images_upload_handler: async (blobInfo) => {
-          const timestamp = new Date().getTime();
-          const filename = `${timestamp}_${blobInfo.filename()}`;
-          const command = new PutObjectCommand({
-            Bucket: ARTICLE_RESOURCES_S3_BUCKET,
-            Key: `images/${filename}`,
-            Body: blobInfo.blob(),
-            ContentType: blobInfo.blob().type,
-          });
-          await s3Client.send(command);
-          return encodeURI(`https://knowtfolio.com/images/${filename}`);
-        },
+        images_upload_handler: imageUploadHandler,
         text_patterns: [
           { start: "*", end: "*", format: "italic" },
           { start: "**", end: "**", format: "bold" },
