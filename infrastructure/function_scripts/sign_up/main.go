02881a497503ff25740b62476692a9b9c50ce82a
Rei Sugiura <reibomaru@gmail.com>
2022-10-10 22:23:37 +0900

    feat: lambdaのiam roleを編集

--- infrastructure/function_scripts/sign_up/main.go
+++ infrastructure/function_scripts/sign_up/main.go
@@ -2,22 +2,33 @@ package main
 
 import (
 	"context"
+	"encoding/json"
+	"net/http"
 	"os"
 
+	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/cognitoidentityprovider"
 )
 
-type SignUpEvent struct {
+type SignUpForm struct {
 	Username      string `json:"username"`
 	Password      string `json:"password"`
 	PhoneNumber   string `json:"phone_number"`
 	WalletAddress string `json:"wallet_address"`
 }
 
-func handler(ctx context.Context, event SignUpEvent) (string, error) {
+func handler(ctx context.Context, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	var signUpForm SignUpForm
+	if err := json.Unmarshal([]byte(request.Body), &signUpForm); err != nil {
+		return events.APIGatewayProxyResponse{
+			StatusCode:      http.StatusBadRequest,
+			IsBase64Encoded: false,
+			Body:            err.Error(),
+		}, nil
+	}
 	// Initialize a session that the SDK will use to load
 	sess := session.Must(session.NewSessionWithOptions(session.Options{
 		SharedConfigState: session.SharedConfigEnable,
@@ -32,24 +43,32 @@ func handler(ctx context.Context, event SignUpEvent) (string, error) {
 		UserAttributes: []*cognitoidentityprovider.AttributeType{
 			{
 				Name:  aws.String("phone_number"),
-				Value: aws.String(event.PhoneNumber),
+				Value: aws.String(signUpForm.PhoneNumber),
 			},
 			{
 				Name:  aws.String("custom:wallet_address"),
-				Value: aws.String(event.WalletAddress),
+				Value: aws.String(signUpForm.WalletAddress),
 			},
 		},
 	}
 
 	newUserData.SetUserPoolId(os.Getenv("USER_POOL_ID"))
-	newUserData.SetUsername(event.Username)
+	newUserData.SetUsername(signUpForm.Username)
 
 	_, err := cognitoClient.AdminCreateUser(newUserData)
 	if err != nil {
-		return "Got error creating user:", err
+		return events.APIGatewayProxyResponse{
+			StatusCode:      http.StatusBadRequest,
+			IsBase64Encoded: false,
+			Body:            err.Error(),
+		}, nil
 	}
 
-	return "OK", nil
+	return events.APIGatewayProxyResponse{
+		StatusCode:      http.StatusOK,
+		IsBase64Encoded: false,
+		Body:            "OK",
+	}, nil
 }
 
 func main() {
