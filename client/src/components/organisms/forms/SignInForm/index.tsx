acf2b7bd05cfa31db76b3008d904a01b4904efd7
Rei Sugiura <reibomaru@gmail.com>
2022-10-28 00:06:45 +0900

    feat: signup formを実装

---
+++ client/src/components/organisms/forms/SignInForm/index.tsx
@@ -0,0 +1,145 @@
+import { Button, Grid } from "@mui/material";
+import { useCallback, useState } from "react";
+import {
+  signInToCognitoWithPassword,
+  signInToCognitoWithWallet,
+} from "~/apis/cognito";
+import Form from "~/components/atoms/authForm/Form";
+import Input from "~/components/atoms/authForm/Input";
+import Label from "~/components/atoms/authForm/Label";
+import Spacer from "~/components/atoms/Spacer";
+import metamaskSvg from "~/components/organisms/forms/SignInForm/metamask.svg";
+import { useWeb3Context } from "~/components/organisms/providers/Web3Provider";
+
+type signInWithPasswordForm = {
+  username: string;
+  password: string;
+};
+
+const SignInForm = () => {
+  const [form, setForm] = useState<signInWithPasswordForm>({
+    username: "",
+    password: "",
+  });
+  const { account, web3 } = useWeb3Context();
+
+  const onChangeForm = useCallback<React.ChangeEventHandler<HTMLInputElement>>(
+    (event) => {
+      switch (event.target.name) {
+        case "username":
+        case "password":
+          setForm((prev) => {
+            return { ...prev, [event.target.name]: event.target.value };
+          });
+          break;
+        default:
+          break;
+      }
+    },
+    []
+  );
+
+  const signInWithPassword = useCallback<
+    React.MouseEventHandler<HTMLButtonElement>
+  >(
+    async (event) => {
+      event.preventDefault();
+      try {
+        await signInToCognitoWithPassword(form.username, form.password);
+        alert("サインイン成功");
+        window.location.reload();
+      } catch (error) {
+        console.error(error);
+        alert("サインイン失敗");
+      }
+    },
+    [form.password, form.username]
+  );
+
+  const signInWithWallet = useCallback<
+    React.MouseEventHandler<HTMLButtonElement>
+  >(
+    async (event) => {
+      event.preventDefault();
+      try {
+        await signInToCognitoWithWallet(form.username, web3, account);
+        alert("サインイン成功");
+        window.location.reload();
+      } catch (error) {
+        console.error(error);
+        alert("サインイン失敗");
+      }
+    },
+    [account, form.username, web3]
+  );
+
+  return (
+    <Form>
+      <h2>サインイン</h2>
+      <hr />
+      <Spacer height="3rem" />
+      <Grid item container spacing={3}>
+        <Input
+          name="username"
+          id="username"
+          onChange={onChangeForm}
+          value={form.username}
+          label="Username"
+          type="text"
+        />
+        <Grid item container direction="row">
+          <Grid item container direction="column" xs={5} spacing={3}>
+            <Input
+              name="password"
+              id="password"
+              onChange={onChangeForm}
+              value={form.password}
+              type="password"
+              label="Password"
+            />
+            <Grid item container justifyContent="center">
+              <Button
+                variant="outlined"
+                onClick={signInWithPassword}
+                style={{ fontSize: "1.4rem" }}
+              >
+                Sign in
+              </Button>
+            </Grid>
+          </Grid>
+          <Grid
+            item
+            container
+            alignItems="center"
+            justifyContent="center"
+            xs={2}
+          >
+            <b>OR</b>
+          </Grid>
+          <Grid item container xs={5}>
+            <Grid item container>
+              <Label>Connected wallet address</Label>
+              <p style={{ margin: 0 }}>{account}</p>
+            </Grid>
+            <Grid item container alignItems="center" justifyContent="center">
+              <Button
+                variant="outlined"
+                onClick={signInWithWallet}
+                style={{ fontSize: "1.4rem" }}
+              >
+                <img
+                  src={metamaskSvg}
+                  alt="metamask_icon"
+                  style={{ height: 40, marginRight: 10 }}
+                />
+                Sign in with metamask
+              </Button>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
+    </Form>
+  );
+};
+
+export default SignInForm;
--- client/src/components/pages/SignInPage/index.tsx
+++ client/src/components/pages/SignInPage/index.tsx
@@ -1,43 +1,42 @@
-import { useCallback, useMemo } from "react";
-import { useNavigate } from "react-router-dom";
-import PasswordSignInForm from "~/components/organisms/forms/PasswordSignInForm";
-import WalletSignInForm from "~/components/organisms/forms/WalletSignInForm";
-import { useQuery } from "~/helpers/reactRouter";
+// import { Grid } from "@mui/material";
+// import { useMemo } from "react";
+// import { useNavigate } from "react-router-dom";
+// import Form from "~/components/atoms/authForm/Form";
+// import Spacer from "~/components/atoms/Spacer";
+import SignInForm from "~/components/organisms/forms/SignInForm";
+// import WalletSignInForm from "~/components/organisms/forms/WalletSignInForm";
+// import { useQuery } from "~/helpers/reactRouter";
 
 const SignInPage = () => {
-  const query = useQuery();
-  const signInMethod = useMemo(() => {
-    return query.get("method") || "password";
-  }, [query]);
-  const navigate = useNavigate();
+  // const query = useQuery();
+  // const signInMethod = useMemo(() => {
+  //   return query.get("method") || "password";
+  // }, [query]);
+  // const navigate = useNavigate();
 
-  const onChangeSigninMethodSelect = useCallback<
-    React.ChangeEventHandler<HTMLSelectElement>
-  >(
-    (event) => {
-      navigate(`/signin?method=${event.target.value}`);
-    },
-    [navigate]
-  );
+  // const onChangeSigninMethodSelect = useCallback<
+  //   React.ChangeEventHandler<HTMLSelectElement>
+  // >(
+  //   (event) => {
+  //     navigate(`/signin?method=${event.target.value}`);
+  //   },
+  //   [navigate]
+  // );
 
-  const signinForm = useMemo(() => {
-    switch (signInMethod) {
-      case "password":
-        return <PasswordSignInForm />;
-      case "wallet":
-        return <WalletSignInForm />;
-      default:
-        return <PasswordSignInForm />;
-    }
-  }, [signInMethod]);
+  // const signinForm = useMemo(() => {
+  //   switch (signInMethod) {
+  //     case "password":
+  //       return <PasswordSignInForm />;
+  //     case "wallet":
+  //       return <WalletSignInForm />;
+  //     default:
+  //       return <PasswordSignInForm />;
+  //   }
+  // }, [signInMethod]);
 
   return (
-    <div>
-      <select value={signInMethod} onChange={onChangeSigninMethodSelect}>
-        <option value="password">パスワードでログイン</option>
-        <option value="wallet">ウォレットでログイン</option>
-      </select>
-      {signinForm}
+    <div style={{ padding: "100px 400px" }}>
+      <SignInForm />
     </div>
   );
 };
