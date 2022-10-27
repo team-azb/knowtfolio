800059fee0c26324ae46db110e0d35c00a26b1cf
Rei Sugiura <reibomaru@gmail.com>
2022-10-28 00:55:42 +0900

    refactor: コメントアウトの消し忘れを削除

--- client/src/components/pages/SignInPage/index.tsx
+++ client/src/components/pages/SignInPage/index.tsx
@@ -1,39 +1,6 @@
-// import { Grid } from "@mui/material";
-// import { useMemo } from "react";
-// import { useNavigate } from "react-router-dom";
-// import Form from "~/components/atoms/authForm/Form";
-// import Spacer from "~/components/atoms/Spacer";
 import SignInForm from "~/components/organisms/forms/SignInForm";
-// import WalletSignInForm from "~/components/organisms/forms/WalletSignInForm";
-// import { useQuery } from "~/helpers/reactRouter";
 
 const SignInPage = () => {
-  // const query = useQuery();
-  // const signInMethod = useMemo(() => {
-  //   return query.get("method") || "password";
-  // }, [query]);
-  // const navigate = useNavigate();
-
-  // const onChangeSigninMethodSelect = useCallback<
-  //   React.ChangeEventHandler<HTMLSelectElement>
-  // >(
-  //   (event) => {
-  //     navigate(`/signin?method=${event.target.value}`);
-  //   },
-  //   [navigate]
-  // );
-
-  // const signinForm = useMemo(() => {
-  //   switch (signInMethod) {
-  //     case "password":
-  //       return <PasswordSignInForm />;
-  //     case "wallet":
-  //       return <WalletSignInForm />;
-  //     default:
-  //       return <PasswordSignInForm />;
-  //   }
-  // }, [signInMethod]);
-
   return (
     <div style={{ padding: "100px 400px" }}>
       <SignInForm />
