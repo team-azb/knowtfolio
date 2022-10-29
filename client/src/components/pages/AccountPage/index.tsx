bc1bd251d62680bf1c811eb0235610805d932623
Rei Sugiura <reibomaru@gmail.com>
2022-10-29 20:18:46 +0900

    feat: mypageのUIを実装

--- client/src/components/pages/AccountPage/index.tsx
+++ client/src/components/pages/AccountPage/index.tsx
@@ -1,7 +1,9 @@
 import { useCallback, useMemo } from "react";
 import { useAuthContext } from "~/components/organisms/providers/AuthProvider";
 import { signOutFromCognito } from "~/apis/cognito";
-import { Link } from "react-router-dom";
+import WalletAddressDisplay from "~/components/organisms/WalletAddressDisplay";
+import { useNavigate } from "react-router-dom";
+import { Button, Grid } from "@mui/material";
 
 const AccountPage = () => {
   const { user, attributes } = useAuthContext();
@@ -20,16 +22,56 @@ const AccountPage = () => {
     window.location.reload();
   }, [user]);
 
+  const navigate = useNavigate();
+
   return (
-    <div>
-      <p>username: {user.getUsername()}</p>
-      <p>phone number: {phoneNumber}</p>
-      <p>
-        wallet address: {walletAddress || "未登録"}{" "}
-        <Link to="/reset-wallet">変更/登録する</Link>
-      </p>
-      <button onClick={signOut}>sign out</button>
-    </div>
+    <Grid
+      container
+      direction="column"
+      spacing={2}
+      style={{ padding: "100px 400px" }}
+    >
+      <Grid item>
+        <h2>Account info</h2>
+        <hr />
+      </Grid>
+      <Grid item container>
+        <Grid xs={2}>Username</Grid>
+        <Grid xs={10}>{user.getUsername()}</Grid>
+      </Grid>
+      <Grid item container>
+        <Grid xs={2}>Phone number</Grid>
+        <Grid xs={10}>{phoneNumber}</Grid>
+      </Grid>
+      <Grid item container alignItems="center">
+        <Grid xs={2}>Wallet address</Grid>
+        <Grid xs={10}>
+          <WalletAddressDisplay
+            style={{ display: "inline" }}
+            address={walletAddress || "未登録"}
+            shouldTruncate={false}
+          />
+          <Button
+            variant="outlined"
+            onClick={() => {
+              navigate("/reset-wallet");
+            }}
+            style={{ marginLeft: "1rem", fontSize: "1.4rem" }}
+          >
+            変更/登録する
+          </Button>
+        </Grid>
+      </Grid>
+      <Grid item>
+        <Button
+          variant="contained"
+          onClick={signOut}
+          style={{ fontSize: "1.4rem" }}
+        >
+          sign out
+        </Button>
+      </Grid>
+    </Grid>
   );
 };
 
