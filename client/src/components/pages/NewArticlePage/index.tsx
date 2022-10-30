0e2756e61ac4495285a21ef6bb79536508bb9de2
Rei Sugiura <reibomaru@gmail.com>
2022-10-31 00:17:05 +0900

    feat: edit article pageのメインのUIを実装

--- client/src/components/pages/NewArticlePage/index.tsx
+++ client/src/components/pages/NewArticlePage/index.tsx
@@ -1,14 +1,13 @@
-import { Grid } from "@mui/material";
 import Spacer from "~/components/atoms/Spacer";
 import NewArticleForm from "~/components/organisms/forms/NewArticleForm";
 
 const NewArticlePage = () => {
   return (
-    <Grid style={{ padding: "100px 400px" }}>
+    <div style={{ padding: "100px 400px" }}>
       <h2>New Aritcle</h2>
       <Spacer height="3rem" />
       <NewArticleForm />
-    </Grid>
+    </div>
   );
 };
 
