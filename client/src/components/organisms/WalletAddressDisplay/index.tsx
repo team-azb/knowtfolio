d3396c7b905b90af2f6ef8ef123f1be345be059c
Rei Sugiura <reibomaru@gmail.com>
2022-10-31 14:52:11 +0900

    refactor: TruncatedTextでリファクタ

--- client/src/components/organisms/WalletAddressDisplay/index.tsx
+++ client/src/components/organisms/WalletAddressDisplay/index.tsx
@@ -1,17 +1,5 @@
 import JetBrainsMono from "~/components/atoms/JetBrainsMono";
-
-/**
- * trancate long string
- * @param str original string
- * @param m Display the first m characters
- * @param n Display the last n characters
- * @returns truncated string
- */
-export const truncate = (str: string, m: number, n: number) => {
-  return str.length > m + n
-    ? str.slice(0, m - 1) + "..." + str.slice(-(n + 1), -1)
-    : str;
-};
+import TrancatedText from "~/components/atoms/TruncatedText";
 
 type walletAddressDisplayProps = {
   address: string;
@@ -32,7 +20,7 @@ const WalletAddressDisplay = ({
 }: walletAddressDisplayProps) => {
   return (
     <JetBrainsMono style={style}>
-      {shouldTruncate ? truncate(address, 8, 4) : address}
+      {shouldTruncate ? <TrancatedText str={address} m={8} n={4} /> : address}
     </JetBrainsMono>
   );
 };
