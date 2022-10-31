ea96b4275ba5e1c83a569eb3a9b87e62e54c7449
Rei Sugiura <reibomaru@gmail.com>
2022-10-31 22:57:11 +0900

    feat: article cardのclick handlerを追加

--- client/src/components/organisms/cards/SearchedArticleCard/index.tsx
+++ client/src/components/organisms/cards/SearchedArticleCard/index.tsx
@@ -2,6 +2,7 @@ import { Grid, GridProps } from "@mui/material";
 import { grey } from "@mui/material/colors";
 import { SearchResultEntry } from "~/apis/knowtfolio";
 import TrancatedText from "~/components/atoms/TruncatedText";
+import WalletAddressDisplay from "~/components/organisms/WalletAddressDisplay";
 
 type searchedArticleCardProps = GridProps & {
   article: SearchResultEntry;
@@ -19,14 +20,18 @@ const SearchedArticleCard = (props: searchedArticleCardProps) => {
       }}
       {...props}
     >
-      <Grid item style={{ height: "10rem" }}>
+      <Grid
+        item
+        style={{ height: "10rem", cursor: "pointer" }}
+        onClick={() => (location.href = `/articles/${props.article.id}`)}
+      >
         <h3 style={{ fontWeight: "normal" }}>
           <TrancatedText str={props.article.title} m={25} n={5} />
         </h3>
       </Grid>
       <Grid item>
         <hr />
-        
+        <WalletAddressDisplay address={props.article.owner_address} />
       </Grid>
     </Grid>
   );
