d1b93c58552e92a9efbd7ed0a44aabf46f75d387
Rei Sugiura <reibomaru@gmail.com>
2022-11-15 14:45:47 +0900

    feat: editorのレイアウトを修正

--- client/src/components/organisms/forms/NewArticleForm/index.tsx
+++ client/src/components/organisms/forms/NewArticleForm/index.tsx
@@ -5,7 +5,7 @@ import { useNavigate } from "react-router-dom";
 import { useWeb3Context } from "~/components/organisms/providers/Web3Provider";
 import ArticleEditor from "~/components/organisms/ArticleEditor";
 import { Button, Grid } from "@mui/material";
-
+import ArrowBackIosNewRoundedIcon from "@mui/icons-material/ArrowBackIosNewRounded";
 /**
  * 記事の新規作成用のフォーム
  */
@@ -56,37 +56,55 @@ const NewArticleForm = () => {
     }
   }, [account, content, navigate, titleInput, web3.eth.personal]);
   return (
-    <Grid container direction="column" spacing={1}>
-      <Grid item>
-        <input
-          style={{
-            border: "2px solid #eee",
-            borderRadius: "0.8rem",
-            boxShadow: "none",
-            boxSizing: "border-box",
-            fontSize: "1.8rem",
-            padding: "1rem",
-            width: "100%",
-          }}
-          type="text"
-          onChange={onChangeTitleInput}
-          value={titleInput}
-          placeholder="Title"
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        position: "absolute",
+        inset: 0,
+      }}
+    >
+      <Grid container direction="row" padding={1}>
+        <Grid item container xs={3} alignItems="center" spacing={1}>
+          <Grid item style={{ cursor: "pointer" }} onClick={() => navigate(-1)}>
+            <ArrowBackIosNewRoundedIcon fontSize="large" />
+          </Grid>
+          <Grid item flexGrow={1}>
+            <input
+              style={{
+                border: "2px solid #eee",
+                borderRadius: "0.8rem",
+                boxShadow: "none",
+                boxSizing: "border-box",
+                fontSize: "1.8rem",
+                padding: "1rem",
+                width: "100%",
+              }}
+              type="text"
+              onChange={onChangeTitleInput}
+              value={titleInput}
+              placeholder="Title"
+            />
+          </Grid>
+        </Grid>
+        <Grid item xs={9} container direction="row-reverse">
+          <Button
+            variant="contained"
+            onClick={handlePost}
+            style={{ fontSize: "1.4rem" }}
+          >
+            create article
+          </Button>
+        </Grid>
+      </Grid>
+      <Grid flexGrow={1}>
+        <ArticleEditor
+          onEditorChange={handleEditorChange}
+          value={content}
+          height="100%"
         />
       </Grid>
-      <Grid item>
-        <ArticleEditor onEditorChange={handleEditorChange} value={content} />
-      </Grid>
-      <Grid item container justifyContent="center">
-        <Button
-          variant="contained"
-          onClick={handlePost}
-          style={{ fontSize: "1.4rem" }}
-        >
-          create article
-        </Button>
-      </Grid>
-    </Grid>
+    </div>
   );
 };
 
