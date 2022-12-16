a0bb4838b47c330d7f0acb615a0d541d6fda6ace
Rei Sugiura <reibomaru@gmail.com>
2022-12-16 16:16:24 +0900

    chore: __isBrowser__の型定義

--- client/types/index.d.ts
+++ client/types/index.d.ts
@@ -8,6 +8,8 @@ interface Window {
     ethereum: any;
 }
 
+// webpackでビルドする際に書き換わる変数
+declare const __isBrowser__: boolean
 declare module "*.module.css" {
     const classes: { readonly [key: string]: string };
     export default classes;
