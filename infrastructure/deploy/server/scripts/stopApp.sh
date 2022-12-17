ac6c86fdd4a6fc581d4e4e08832adc3ec20c26e6
Rei Sugiura <reibomaru@gmail.com>
2022-12-17 16:23:07 +0900

    refactor: remove -9 option from kill command

--- infrastructure/deploy/server/scripts/stopApp.sh
+++ infrastructure/deploy/server/scripts/stopApp.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 PID=$(lsof -t -i :8080)
 if [ -n "$PID" ]; then
-  kill -9 $PID
-  echo "kill server PID:$PID"
+  kill $PID
+  echo "stopped server PID:$PID"
 else
-  echo "no server to kill"
+  echo "no server to stop"
 fi
 exit 0
