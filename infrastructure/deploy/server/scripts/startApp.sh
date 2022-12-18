f24f9574f906d371ab437999f4cb8250b6c551ab
Rei Sugiura <reibomaru@gmail.com>
2022-12-18 16:40:58 +0900

    refactor: 標準入力に/dev/nullを指定

--- infrastructure/deploy/server/scripts/startApp.sh
+++ infrastructure/deploy/server/scripts/startApp.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 APP_ROOT=/home/ubuntu/knowtfolio
 export $(cat $APP_ROOT/.env | grep -v '^#' | xargs)
-nohup $APP_ROOT/server > $APP_ROOT/knowtfolio.out.log 2> $APP_ROOT/knowtfolio.err.log &
+nohup $APP_ROOT/server > $APP_ROOT/knowtfolio.out.log 2> $APP_ROOT/knowtfolio.err.log < /dev/null &
