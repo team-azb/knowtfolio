976c6552e0a50ec40f5e014d5d7f53d1adfed084
Rei Sugiura <reibomaru@gmail.com>
2022-12-21 19:15:00 +0900

    style: add comment

--- client/src/csr.tsx
+++ client/src/csr.tsx
@@ -3,7 +3,7 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 
 const container = document.getElementById("app") as HTMLElement;
-const root = createRoot(container);
+const root = createRoot(container); // createRoot(container!) if you use TypeScript
 root.render(
   <BrowserRouter>
     <App />
