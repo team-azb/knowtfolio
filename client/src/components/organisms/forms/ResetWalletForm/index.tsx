578fb479953703e5408ddf098d77f70416b7fe7b
Rei Sugiura <reibomaru@gmail.com>
2023-01-07 12:51:20 +0900

    feat: /api/wallet_addressを実装

--- client/src/components/organisms/forms/ResetWalletForm/index.tsx
+++ client/src/components/organisms/forms/ResetWalletForm/index.tsx
@@ -1,4 +1,3 @@
-import { CognitoUserAttribute } from "amazon-cognito-identity-js";
 import { useCallback, useState } from "react";
 import { RadioGroup, Radio, FormControlLabel } from "@mui/material";
 import { useAuthContext } from "~/components/organisms/providers/AuthProvider";
@@ -8,6 +7,7 @@ import WalletAddressDisplay from "~/components/organisms/WalletAddressDisplay";
 import { Button, Grid } from "@mui/material";
 import Spacer from "~/components/atoms/Spacer";
 import { useNavigate } from "react-router-dom";
+import { postWalletAddress } from "~/apis/lambda";
 
 /**
  * walletを再設定するためのフォーム
@@ -15,26 +15,28 @@ import { useNavigate } from "react-router-dom";
 const ResetWalletForm = () => {
   const [walletAddressInput, setWalletAddressInput] = useState<string>("");
   const { user, userWalletAddress } = useAuthContext();
-  const { account } = useWeb3Context();
+  const { account, web3 } = useWeb3Context();
 
-  const resetWalletAddress = useCallback(() => {
-    user.updateAttributes(
-      [
-        new CognitoUserAttribute({
-          Name: "custom:wallet_address",
-          Value: walletAddressInput,
-        }),
-      ],
-      (err) => {
-        if (err) {
-          alert(err.message || JSON.stringify(err));
-          return;
-        }
-        alert("wallet addressの更新に成功しました");
-        window.location.reload();
-      }
+  const resetWalletAddress = useCallback(async () => {
+    const signature = await web3.eth.personal.sign(
+      "Update Wallet Address",
+      account,
+      ""
     );
-  }, [user, walletAddressInput]);
+    try {
+      await postWalletAddress({
+        userId: user.getUsername(),
+        walletAddress: walletAddressInput,
+        signature: signature,
+      });
+      // TODO: toastで実装する
+      alert("Walletの登録が完了しました。")
+      window.location.reload()
+    } catch (error) {
+      // TODO: toastで実装する
+      alert("Walletの登録に失敗しました。")
+    }
+  }, [account, user, walletAddressInput, web3.eth.personal]);
 
   const onChangeWalletAddressInput = useCallback<
     React.ChangeEventHandler<HTMLInputElement>
@@ -46,7 +48,7 @@ const ResetWalletForm = () => {
 
   return (
     <Form>
-      <h2>Wallet addressを再設定する</h2>
+      <h2>Wallet addressを登録する</h2>
       <hr />
       <Spacer height="3rem" />
       <Grid container direction="column" spacing={3}>
