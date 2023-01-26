84a108a6abed057b4289ad4e201cfcbcdb754894
Rei Sugiura <reibomaru@gmail.com>
2023-01-26 20:56:29 +0900

    feat: AccountInfoTableにアイコンを表示

--- client/src/components/organisms/tables/AccountInfoTable/index.tsx
+++ client/src/components/organisms/tables/AccountInfoTable/index.tsx
@@ -5,11 +5,12 @@ import WalletAddressDisplay from "~/components/organisms/WalletAddressDisplay";
 import { useNavigate } from "react-router-dom";
 import { Button, Grid } from "@mui/material";
 import { toast } from "react-toastify";
+import IconImage from "~/components/atoms/IconImage";
 
 const AccountInfoTable = () => {
   const {
     user,
-    attributes: { phoneNumber, email, website, description },
+    attributes: { phoneNumber, email, website, description, picture },
     userWalletAddress,
   } = useAuthContext();
   const navigate = useNavigate();
@@ -33,33 +34,42 @@ const AccountInfoTable = () => {
         <h2>Account info</h2>
         <hr />
       </Grid>
-      <Grid item container>
-        <Grid xs={2}>Username</Grid>
-        <Grid xs={10}>{user.getUsername()}</Grid>
-      </Grid>
-      <Grid item container>
-        <Grid xs={2}>Phone number</Grid>
-        <Grid xs={10}>{phoneNumber}</Grid>
-      </Grid>
-      <Grid item container>
-        <Grid xs={2}>Email</Grid>
-        <Grid xs={10}>{email || "-"}</Grid>
-      </Grid>
-      <Grid item container>
-        <Grid xs={2}>Website</Grid>
-        <Grid xs={10}>
-          {website ? (
-            <a href={website} style={{ color: "#000" }}>
-              {website}
-            </a>
-          ) : (
-            "-"
-          )}
+      <Grid item container spacing={5}>
+        <Grid item>
+          <IconImage url={picture} />
         </Grid>
-      </Grid>
-      <Grid item container>
-        <Grid xs={2}>Biography</Grid>
-        <Grid xs={10}>{description || "-"}</Grid>
+        <Grid item flexGrow={1}>
+          <Grid container direction="column" spacing={2}>
+            <Grid item container>
+              <Grid xs={2}>Username</Grid>
+              <Grid xs={10}>{user.getUsername()}</Grid>
+            </Grid>
+            <Grid item container>
+              <Grid xs={2}>Phone number</Grid>
+              <Grid xs={10}>{phoneNumber}</Grid>
+            </Grid>
+            <Grid item container>
+              <Grid xs={2}>Email</Grid>
+              <Grid xs={10}>{email || "-"}</Grid>
+            </Grid>
+            <Grid item container>
+              <Grid xs={2}>Website</Grid>
+              <Grid xs={10}>
+                {website ? (
+                  <a href={website} style={{ color: "#000" }}>
+                    {website}
+                  </a>
+                ) : (
+                  "-"
+                )}
+              </Grid>
+            </Grid>
+            <Grid item container>
+              <Grid xs={2}>Biography</Grid>
+              <Grid xs={10}>{description || "-"}</Grid>
+            </Grid>
+          </Grid>
+        </Grid>
       </Grid>
       <Grid item container spacing={1}>
         <Grid item>
