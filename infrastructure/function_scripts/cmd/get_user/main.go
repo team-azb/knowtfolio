13b2add53afeed6ff789428aff72620353746b14
Rei Sugiura <reibomaru@gmail.com>
2023-02-02 23:41:18 +0900

    feat: get_userのawsリソースを作成

---
+++ infrastructure/function_scripts/cmd/get_user/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"context"
+	"net/http"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+func handler(ctx context.Context, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	return events.APIGatewayProxyResponse{
+		StatusCode: http.StatusOK,
+	}, nil
+}
+
+func main() {
+	lambda.Start(handler)
+}
