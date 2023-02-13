9b635ef1cc84cd65920633c006beaffdc14d1f04
shugo256 <shugo256@gmail.com>
2023-02-13 22:00:28 +0900

    Validate cognito token in `post_wallet_address`.

--- infrastructure/function_scripts/cmd/post_wallet_address/main.go
+++ infrastructure/function_scripts/cmd/post_wallet_address/main.go
@@ -12,20 +12,47 @@ import (
 	"github.com/aws/smithy-go"
 	"github.com/team-azb/knowtfolio/infrastructure/function_scripts/pkg/aws_utils"
 	"github.com/team-azb/knowtfolio/infrastructure/function_scripts/pkg/models"
+	awsutil "github.com/team-azb/knowtfolio/server/gateways/aws"
 	"net/http"
+	"strings"
 )
+
+var cognitoClient = awsutil.NewCognitoClient()
 
 func handler(ctx context.Context, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
 	fmt.Printf("Put Wallet Address: %+v\n", request)
 
+	authHeader, ok := request.Headers["authorization"]
+	if !ok {
+		return events.APIGatewayProxyResponse{
+			StatusCode: http.StatusUnauthorized,
+			Body:       "authorization header is required",
+		}, nil
+	}
+
+	token := strings.TrimPrefix(authHeader, "Bearer ")
+	tokenUserID, err := cognitoClient.VerifyCognitoToken(token)
+	if err != nil {
+		return events.APIGatewayProxyResponse{
+			StatusCode: http.StatusUnauthorized,
+			Body:       "invalid token",
+		}, nil
+	}
+
 	var req models.PostWalletAddressRequest
 
 	// Validate request body format.
-	err := models.ValidateJSONRequest([]byte(request.Body), &req)
+	err = models.ValidateJSONRequest([]byte(request.Body), &req)
 	if err != nil {
 		return events.APIGatewayProxyResponse{
 			StatusCode: http.StatusBadRequest,
 			Body:       err.Error(),
+		}, nil
+	}
+	if req.UserID != tokenUserID {
+		return events.APIGatewayProxyResponse{
+			StatusCode: http.StatusForbidden,
+			Body:       fmt.Sprintf("user [%v] cannot register user [%v]'s wallet address", tokenUserID, req.UserID),
 		}, nil
 	}
 
